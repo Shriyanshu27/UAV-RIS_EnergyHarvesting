76f1d37d727ce60a62da6d6b726ae18c8f4f6b37
Shriyanshu27 <114811340+Shriyanshu27@users.noreply.github.com>
2023-08-03 14:58:55 +0530

    Update PPO.py

--- PPO/PPO.py
+++ PPO/PPO.py
@@ -12,7 +12,7 @@ env = gym.make('foo-v0')
 n_actions = env.action_space.shape[-1]
 
 model = PPO("MlpPolicy", env, learning_rate=1e-3, n_steps=pow(2, 10), batch_size=pow(2, 10), verbose=1, tensorboard_log="./PPO_MultiUT_Time_tensorboard/")
-model.learn(total_timesteps=41000, log_interval=10)
+model.learn(total_timesteps=820000, log_interval=10)
 model.save("ppo_MultiUT_Time")
 env = model.get_env()
 
